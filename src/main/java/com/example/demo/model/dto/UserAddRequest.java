9fb8ef6a9b875a37268640d172bb1a8a93759e85
tegarrramadhaaan <anakrama2000@gmail.com>
2026-03-03 10:41:07 +0700

    penambahan di UserAddRequest

---
+++ src/main/java/com/example/demo/model/dto/UserAddRequest.java
@@ -0,0 +1,10 @@
+package com.example.demo.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserAddRequest {
+    private String name;
+
+    private  int age;
+}
--- src/main/java/com/example/demo/model/dto/UserDto.java
+++ src/main/java/com/example/demo/model/dto/UserDto.java
@@ -1,4 +1,4 @@
-package com.example.demo.model.dto.user;
+package com.example.demo.model.dto;
 
 import lombok.Data;
 
